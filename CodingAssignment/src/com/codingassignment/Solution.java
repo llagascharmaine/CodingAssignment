d5478d0bef4385edb86a798cae2fd69559429891
llagascharmaine <87605461+llagascharmaine@users.noreply.github.com>
2021-07-18 22:34:52 +0800

    Solution for D

--- CodingAssignment/src/com/codingassignment/Solution.java
+++ CodingAssignment/src/com/codingassignment/Solution.java
@@ -18,9 +18,35 @@ public class Solution {
 		Fish shark = new Fish("large", "grey", "eat other fish");
 		Fish clownFish = new Fish("small", "orange", "makes joke");
 		Dolphin dolphin = new Dolphin();
-
 		Butterfly butterfly = new Butterfly("butterfly");
 		Butterfly caterpillar = new Butterfly("caterpillar");
+		
+		Animal[] animals = new Animal[] { bird, duck, chicken, rooster, parrot1, parrot2, parrot3, shark, clownFish,
+				dolphin, butterfly, caterpillar };
+
+		int canWalkCount = 0;
+		int canFlyCount = 0;
+		int canSingCount = 0;
+		int canSwimCount = 0;
+
+		for (Animal animal : animals) {
+			if (animal.isCanWalk())
+				canWalkCount++;
+
+			if (animal.isCanFly())
+				canFlyCount++;
+
+			if (animal.isCanSing())
+				canSingCount++;
+
+			if (animal.isCanSwim())
+				canSwimCount++;
+		}
+
+		System.out.println("No. of animals that can walk: " + canWalkCount);
+		System.out.println("No. of animals that can fly: " + canFlyCount);
+		System.out.println("No. of animals that can sing: " + canSingCount);
+		System.out.println("No. of animals that can swim: " + canSwimCount);
 	}
 }
 
